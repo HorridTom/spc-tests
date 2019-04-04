2657052a27dfea60f710bdfd4ac8cb2a54ed0f63
Eftychiou <lle15@ic.ac.uk>
2019-04-04 13:28:35 +0100

    Test analysis of C and I charts

--- R/C_Chart_analysis.R
+++ R/C_Chart_analysis.R
@@ -5,3 +5,15 @@ library(tidyverse)
 dfc <- data_list[["Spc_C"]]
 #delete the blank column 
 dfc <- dfc %>% select(-X__1)
+
+#Load qicharts2
+library(qicharts2)
+#Create C Chart from the data 
+test<- qic(dfc$month,dfc$`Process Data Points`,
+    data  = dfc,
+    chart = 'c',
+    title = 'Hospital acquired Clostridium difficile infections (C chart)',
+    ylab  = 'Count',
+    xlab  = 'Month')
+#Pull out data from the c chart (UCL, LCL, CL)
+test2<- test$data
---
+++ R/I_Chart_analysis.R
@@ -0,0 +1,29 @@
+#Test analysis of the I chart 
+library(tidyverse)
+
+#pick out chart
+dfi <- data_list[["Spc_I"]]
+#view chart to identify empty column 
+View(dfi)
+#delete empty column 
+dfi <- dfi %>% select(-X__2)
+#create I chart from data
+testi<- qic(dfi$X__1,
+    data      = dfi,
+    chart     = 'i',
+    show.grid = TRUE,
+    title     = '(I chart)',
+    ylab      = 'Years',
+    xlab      = 'Patient #')
+#extract underlying data from chart 
+test3<-testi$data
+#create I chart with moving ranges
+test3i<-qic(dfi$X__1,
+    data      = dfi, 
+    chart     = 'mr',
+    show.grid = TRUE,
+    title     = 'Age of the last 100 patients (MR chart)',
+    ylab      = 'Years',
+    xlab      = 'Patient #')
+#extract data from I chart with moving ranges
+test4<-test3i$data
