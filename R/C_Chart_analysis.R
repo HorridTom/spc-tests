2fdb22b9164b803437fafd5fbabdb5f4b2eb67ad
Eftychiou <lle15@ic.ac.uk>
2019-04-09 10:11:32 +0100

    added code to save c chart results to disc

--- R/C_Chart_analysis.R
+++ R/C_Chart_analysis.R
@@ -8,12 +8,22 @@ dfc <- dfc %>% select(-X__1)
 
 #Load qicharts2
 library(qicharts2)
+#create row 'month'
+dfc$month<- 1:nrow(dfc) 
 #Create C Chart from the data 
-test<- qic(dfc$month,dfc$`Process Data Points`,
+c_chart<- qic(dfc$month,dfc$`Process Data Points`,
     data  = dfc,
     chart = 'c',
     title = 'Hospital acquired Clostridium difficile infections (C chart)',
     ylab  = 'Count',
     xlab  = 'Month')
 #Pull out data from the c chart (UCL, LCL, CL)
-test2<- test$data
+c_chart_data<- test$data
+c_chart
+
+
+#save results 
+c_chart_results<- c_chart_data %>% select(y, cl, lcl, ucl) %>%
+  rename(`Process Data Points`= y, `Process Averages` = cl, `Process LCLs` = lcl, `Process UCLs` = ucl)
+
+write_csv(c_chart_results, path = "data-out/c_chart_results.csv")
