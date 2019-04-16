d6731a45ccc1611c2af7d407372cb2a78a9fec65
lle15 <lle15@ic.ac.uk>
2019-04-16 10:12:16 +0100

    results for I chart and I chart with moving ranges

--- R/I_Chart_analysis.R
+++ R/I_Chart_analysis.R
@@ -8,7 +8,7 @@ View(dfi)
 #delete empty column 
 dfi <- dfi %>% select(-X__2)
 #create I chart from data
-testi<- qic(dfi$X__1,
+i_chart<- qic(dfi$X__1,
     data      = dfi,
     chart     = 'i',
     show.grid = TRUE,
@@ -16,9 +16,18 @@ testi<- qic(dfi$X__1,
     ylab      = 'Years',
     xlab      = 'Patient #')
 #extract underlying data from chart 
-test3<-testi$data
-#create I chart with moving ranges
-test3i<-qic(dfi$`Range Data Points`,
+i_chart_data<-i_chart$data
+
+
+#save results for I chart
+i_chart_results<- i_chart_data %>% select(y, cl, lcl, ucl) %>%
+  rename(`Process Data Points`= y, `Process Averages` = cl, `Process LCLs` = lcl, `Process UCLs` = ucl)
+
+write_csv(i_chart_results, path = "data-out/c_chart_results.csv")
+
+
+#create I chart with moving ranges (MR)
+i_chart_MR<-qic(dfi$`Range Data Points`,
     data      = dfi, 
     chart     = 'mr',
     show.grid = TRUE,
@@ -26,4 +35,11 @@ test3i<-qic(dfi$`Range Data Points`,
     ylab      = 'Years',
     xlab      = 'Patient #')
 #extract data from I chart with moving ranges
-test4<-test3i$data
+i_chart_MR_data<-i_chart_MR$data
+
+
+#save results for I chart with moving ranges (MR)
+i_chart_MR_results<- i_chart_MR_data %>% select(y.sum, cl, lcl, ucl) %>%
+  rename(`Range Data Points`= y.sum, `Range Averages` = cl, `Range LCLs` = lcl, `Range UCLs` = ucl)
+
+write_csv(i_chart_MR_results, path = "data-out/i_chart_MR_results.csv")
