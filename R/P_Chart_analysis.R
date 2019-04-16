f8f70febd431e45d61e1c11c7e50419b8f596269
lle15 <lle15@ic.ac.uk>
2019-04-16 10:16:35 +0100

    Results for P chart

--- R/P_Chart_analysis.R
+++ R/P_Chart_analysis.R
@@ -1,11 +1,20 @@
 dfp <- data_list[["Spc_P"]]
 dfp <- dfp %>% select(-X__1)
 dfp$month<- 1:nrow(dfp) 
-testa<-qic(month, dfp$`Numerator (COUNT)`, dfp$`Denominator (COUNT)`,
+p_chart<-qic(month, dfp$`Numerator (COUNT)`, dfp$`Denominator (COUNT)`,
     data      = dfp,
     chart     = 'p',
     y.percent = TRUE,
     title     = 'Harmed patients (P chart)',
     ylab      = NULL,
     xlab      = 'Month')
-testaa<-testa$data
+p_chart_data<-p_chart$data
+
+
+#save results 
+p_chart_results<- p_chart_data %>% select(y, cl, lcl, ucl) %>%
+  rename(`Process Data Points`= y, `Process Averages` = cl, `Process LCLs` = lcl, `Process UCLs` = ucl)
+
+write_csv(p_chart_results, path = "data-out/p_chart_results.csv")
+
+
