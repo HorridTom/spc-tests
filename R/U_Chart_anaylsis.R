36cf7149702bfb800d49a4309e3f99b00d3e9f30
lle15 <lle15@ic.ac.uk>
2019-04-16 11:10:59 +0100

    U Chart data results

--- R/U_Chart_anaylsis.R
+++ R/U_Chart_anaylsis.R
@@ -1,2 +1,28 @@
+
+# pick out the U chart data 
 dfu <- data_list[["Spc_U"]]
+
+# delete empty column 
 dfu <- dfu %>% select(-X__1)
+
+# add column month 
+dfu$month<- 1:nrow(dfu)
+
+# create u chart 
+u_chart<- qic(dfu$month, dfu$`Numerator (COUNT)`, dfu$`Denominator (SUM)`,
+           data     = dfu,
+           chart    = 'u',
+           multiply = 1,
+           title    = 'Patient harm (U chart)',
+           ylab     = 'Adverse events per day',
+           xlab     = 'Month')
+
+# extract data from u chart
+u_chart_data<- u_chart$data
+u_chart
+
+# save results for u chart
+u_chart_results<- u_chart_data %>% select(y, cl, lcl, ucl) %>%
+  rename(`Process Data Points`= y, `Process Averages` = cl, `Process LCLs` = lcl, `Process UCLs` = ucl)
+
+write_csv(u_chart_results, path = "data-out/u_chart_results.csv")
